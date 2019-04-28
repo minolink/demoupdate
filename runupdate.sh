951bb1c3c6eaf4fca770baffbc3544ec64d39b5a
root <minolink@hotmail.com>
2019-04-28 16:41:00 +0800

    update sh update

--- runupdate.sh
+++ runupdate.sh
@@ -5,9 +5,11 @@ d1=$(cat /home/pi/demoapp/ver.md)
 
 if [ $d2>$d1 ]; then
 	echo "begin update..."
-        unzip /opt/demoupdate/demoupdate/ud.zip
-	mv /home/pi/demoapp/demoapp /home/pi/demoapp_bk
-        cp /opt/demoupdate/demoupdate/demoapp /home/pi/demoapp/	
+        unzip -o /opt/demoupdate/demoupdate/ud.zip
+	mv /home/pi/demoapp/demoapp /home/pi/demoapp/demoapp_bk
+        cp /opt/demoupdate/demoupdate/demoapp /home/pi/demoapp/
+        mv /home/pi/demoapp/ver.md /home/pi/demoapp/ver_bk.md
+        cp /opt/demoupdate/demoupdate/ver.md /home/pi/demoapp	
 	echo "update finish"
 fi
 
